368b68c4d59881a35f48f59372b89c641d4bf073
James Williams <jamesrwilliams91@gmail.com>
2015-07-09 23:56:00 -0700

    update edit and add functinoality for user to include name with devise

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,7 @@
 Rails.application.routes.draw do
   devise_for :users
-  root 'users#index'
+  root 'about#index'
+  resources :about, :only => [:index]
   resources :users, :only => [:show, :index]
   resources 'categories' do
     resources 'projects', except: :index
