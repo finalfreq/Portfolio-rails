ca9652ce52c0b882c53ea9d8a8528388e3829bcb
James Williams <jamesrwilliams91@gmail.com>
2015-07-17 22:48:46 -0700

    fix bug with spec not passing for validations, change path that was checked

--- spec/features/validate_sign_in_paths_spec.rb
+++ spec/features/validate_sign_in_paths_spec.rb
@@ -2,7 +2,7 @@ require 'rails_helper'
 
 describe "Unauthenticated user path" do
   it 'redirects user to sign in page ' do
-    visit categories_path
+    visit users_path
     expect(page).to have_content 'sign up'
   end
 end
